e735b257a1763e8ce063c534495a06bbdfc7ece0
cheungbh <534660436@qq.com>
2019-08-14 20:20:53 -0700

    Can get data without preocessing video

---
+++ coord_process.py
@@ -0,0 +1,49 @@
+from config import config
+import os
+
+from data_process.process_txt import txtProcessor, txtMerger
+from tools.final_merge import merge
+from tools.label import write_label
+
+class_ls = config.class_ls
+main_folder = config.folder_path
+step = config.step
+frame = config.frame
+data_folder = os.path.join("data", config.action, config.folder, "{}frames_{}step".format(frame, step))
+
+if __name__ == '__main__':
+    cnt = 1
+    for cls in class_ls:
+        print("{}/{}".format(cnt, len(class_ls)))
+        print('Begin to process the videos of {}'.format(cls))
+        action_folder = os.path.join(main_folder, cls)
+        os.makedirs(data_folder, exist_ok=True)
+
+        coord_path = os.path.join(main_folder, cls, "output", "coordinate")
+        print("Begin to transfer coordinate into the form of input data")
+        # (txt)coordinate to (txt)data
+        txt_folder = os.path.join(data_folder, cls)
+        act_txtP = txtProcessor(coord_path, txt_folder, step, frame)
+        act_txtP.run()
+        data_path = os.path.join(data_folder, "{}.txt".format(cls))
+        print('Finish transferring from coordinate to input data')
+
+        print("Begin to merge the input data of {}".format(cls))
+        # Merge data within a class
+        txtM = txtMerger(txt_folder, data_path)
+        txtM.run()
+
+        print("Finish merging; Begin to produce label")
+
+        # Write the label
+        write_label(data_path, cnt)
+        cnt += 1
+
+        print("Finish processing {}".format(cls))
+        print("\n")
+        print("\n")
+
+    merge(data_folder)
+
+
+
---
+++ video_process.py
@@ -0,0 +1,66 @@
+from config import config
+import os
+
+from data_process.process_video import VideoProcessor
+from data_process.process_json import JsonScanner
+from data_process.process_txt import txtProcessor, txtMerger
+from tools.final_merge import merge
+from tools.label import write_label
+
+class_ls = config.class_ls
+main_folder = config.folder_path
+step = config.step
+frame = config.frame
+data_folder = os.path.join("data", config.action, config.folder, "{}frames_{}step".format(frame, step))
+
+
+if __name__ == '__main__':
+    print("Begin to process the videos in {}".format(main_folder))
+    cnt = 1
+    for cls in class_ls:
+        print("{}/{}".format(cnt, len(class_ls)))
+        print('Begin to process the videos of {}'.format(cls))
+        action_folder = os.path.join(main_folder, cls)
+        os.makedirs(data_folder, exist_ok=True)
+
+        # Video to json
+        act_VP = VideoProcessor(action_folder)
+        act_VP.run_video()
+        print("Finish processing the videos of {}".format(cls))
+
+        print("Begin to transfer json to normalized coordinate")
+        json_path = act_VP.output_path
+        coord_path = os.path.join(json_path, "coord_file")
+        os.makedirs(coord_path, exist_ok=True)
+        # json to (txt)coordinate
+        act_JS = JsonScanner(json_path, coord_path)
+        act_JS.run()
+        print("Finish transferring to coordinate")
+
+        print("Begin to transfer coordinate into the form of input data")
+        # (txt)coordinate to (txt)data
+        txt_folder = os.path.join(data_folder, cls)
+        act_txtP = txtProcessor(coord_path, txt_folder, step, frame)
+        act_txtP.run()
+        data_path = os.path.join(data_folder, "{}.txt".format(cls))
+        print('Finish transferring from coordinate to input data')
+
+        print("Begin to merge the input data of {}".format(cls))
+        # Merge data within a class
+        txtM = txtMerger(txt_folder, data_path)
+        txtM.run()
+
+        print("Finish merging; Begin to produce label")
+
+        # Write the label
+        write_label(data_path, cnt)
+        cnt += 1
+
+        print("Finish processing {}".format(cls))
+        print("\n")
+        print("\n")
+
+    merge(data_folder)
+
+
+
